c4516884d6dc73812dec49d025816f8baaa48cf4
micro-machine <croteaumj@engr2-17-50-dhcp.int.colorado.edu>
2014-09-24 15:46:00 -0600

    scrypt and pos updates
    (some commits from sdcdev/insight)

--- util/util.js
+++ util/util.js
@@ -3,6 +3,7 @@ var bignum = require('bignum');
 var Binary = require('binary');
 var Put = require('bufferput');
 var buffertools = require('buffertools');
+var node_scrypt = require('scryptsy').scrypt;
 var sjcl = require('../lib/sjcl');
 if (process.browser) {
   var hashjs = require('hash.js');
@@ -56,6 +57,11 @@ var twoSha256 = exports.twoSha256 = function(data) {
 
 var sha256ripe160 = exports.sha256ripe160 = function(data) {
   return ripe160(sha256(data));
+};
+
+var scrypt = exports.scrypt = function(data) {
+  // scrypt(passwd, salt, N, r, p, dkLen)
+  return node_scrypt(data, data, 1024, 1, 1, 32);
 };
 
 /**
